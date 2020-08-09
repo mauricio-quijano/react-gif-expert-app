04f34028036160320f467f65993abd5f2dbc8f4a
Mauricio Quijano <mauricioquijano@gmail.com>
2020-08-09 16:52:26 -0500

    added docs

---
+++ docs/precache-manifest.d8c7640c8e145dc167a4c6c7789e1a25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e57d355433fb95371ad4a690d0c117a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc766f1e4638c90c2f27",
+    "url": "/static/css/main.4b4dd1e1.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc766f1e4638c90c2f27",
+    "url": "/static/js/main.fb5ea26f.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
